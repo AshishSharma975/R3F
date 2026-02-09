35033a4d75913a59234005bbf8ea290397f5ae6c
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-09 15:36:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { AmbientLight, CylinderGeometry } from "three";
+import * as THREE from "three"
+const App = () => {
+  return (
+    <Canvas>
+      <OrbitControls />
+      <ambientLight />
+      <mesh>
+        <cylinderGeometry args={[ 1, 1, 1 ,30,30,true]} />
+        <meshStandardMaterial side={THREE.DoubleSide} />
+      </mesh>
+    </Canvas>
+  );
+};
+
+export default App;
