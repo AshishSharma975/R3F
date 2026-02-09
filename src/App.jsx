9d55e83aa959a0794cc4d333023c735a5a6b191e
AshishSharma975 <ashish8389sharma@gmail.com>
2026-02-09 16:43:00 +0530

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import Scene from "./Scene";
-
+import { Bloom, ToneMapping } from "@react-three/postprocessing";
+import { EffectComposer } from "@react-three/postprocessing";
 const App = () => {
   return (
-    <Canvas>
+    <>
+    <Canvas flat camera={{fov:70}}>
       <OrbitControls />
       <ambientLight />
       <Scene />
+       <EffectComposer>
+        <Bloom
+        mipmapBlur
+          intensity={1.0}          
+          luminanceThreshold={0} 
+          luminanceSmoothing={0}
+        />
+      </EffectComposer>
     </Canvas>
+
+
+</>
+
   );
+
+
 };
 
 export default App;
--- src/Scene.jsx
+++ src/Scene.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTexture } from "@react-three/drei";
 import * as THREE from "three";
+import { useFrame } from "@react-three/fiber";
 
 const Scene = () => {
-  let tex = useTexture("/image.png");  // public/image.png hona chahiye
+  let tex = useTexture("/image.png");
+
+
+
+  let cyl = useRef(null);
+  useFrame((state, delta) => {
+    cyl.current.rotation.y += delta;
+  });
 
   return (
-    <mesh>
-      <cylinderGeometry args={[1, 1, 1, 30, 30, true]} />
-      <meshStandardMaterial
-        map={tex}
-        transparent
-        side={THREE.DoubleSide}
-      />
-    </mesh>
+    <group rotation={[0, 1.4, 0.5]}>
+      <mesh ref={cyl}>
+        <cylinderGeometry args={[2, 2, 2, 60, 60, true]} />
+        <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+      </mesh>
+    </group>
   );
 };
 
